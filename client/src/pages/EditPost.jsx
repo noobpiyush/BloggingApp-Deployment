21dd0324c35d7c068b20835166a0421621dadb47
noobpiyush <83893659+noobpiyush@users.noreply.github.com>
2024-02-03 12:43:32 +0530

    Update EditPost.jsx

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -36,7 +36,7 @@ export default function EditPost() {
         if (files?.[0]) {
             data.set("file", files?.[0]);
         }
-        const response = await fetch("http://localhost:4000/post", {
+        const response = await fetch("http://65.0.127.223:4000/post", {
             method: "PUT",
             body: data,
             credentials:"include"
