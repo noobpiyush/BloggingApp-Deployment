6aa014fe99fb2bfc1c7b4d799b4d230e0aad54b1
noobpiyush <83893659+noobpiyush@users.noreply.github.com>
2024-02-03 12:42:13 +0530

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,7 +17,7 @@ export default function CreatePost() {
         data.set("file",files[0]);
         ev.preventDefault();
         
-      const response =  await fetch("http://localhost:4000/post",{
+      const response =  await fetch("http://65.0.127.223:4000/post",{
             method:"POST",
             body: data,
             credentials:"include",
