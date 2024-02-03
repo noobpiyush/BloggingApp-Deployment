b274b62a88e1338204499839a195c61651e3ca0b
noobpiyush <83893659+noobpiyush@users.noreply.github.com>
2024-02-03 12:41:40 +0530

    Update Posts.jsx

--- client/src/Posts.jsx
+++ client/src/Posts.jsx
@@ -5,7 +5,7 @@ export default function Posts({ _id ,title, summary, cover, content, createdAt, 
     <div className='post'>
       <div className="image">
         <Link to={`/post/${_id}`} >
-          <img src={"http://localhost:4000/" + cover} alt="" />
+          <img src={"hhttp://65.0.127.223:4000/" + cover} alt="" />
         </Link>
       </div>
       <div className="text">
